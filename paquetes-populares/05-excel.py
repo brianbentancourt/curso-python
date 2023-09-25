3076255b3d8f4a9c4cd2d51c8a01c244cb08f017
Brian Bentancourt <brian.bentancourt@sonda.com>
2023-09-25 14:56:08 -0300

    web scraping y trabajando con archivos xlsx

---
+++ paquetes-populares/05-excel.py
@@ -0,0 +1,44 @@
+import openpyxl
+
+wb = openpyxl.load_workbook("./Planilla.xlsx")
+# print(wb.sheetnames)
+# print(wb["Hoja1"])
+
+hoja = wb.active
+wb.create_sheet("Hoja3")
+hoja3 = wb["Hoja3"]
+hoja3.title = "Nuevo titulo"
+
+# print(
+#     hoja.max_row,
+#     hoja.max_column
+# )
+
+celda = hoja["A1"]
+celda.value = "Nombre completo"
+# print(celda.value)
+
+celda2 = hoja.cell(row=2, column=1)
+# print(
+#     celda2.value,
+#     celda2.row,
+#     celda2.column,
+#     celda2.coordinate
+# )
+
+for fila in range(1, hoja.max_row):
+    for columna in range(1, hoja.max_column + 1):
+        celda = hoja.cell(row=fila, column=columna)
+        # print(fila, columna, celda.value)
+
+columna = hoja["A"]
+fila = hoja["1"]
+# print(columna)
+# print(fila)
+
+hoja.append([1, 2, 3])
+# print(hoja.rows)
+
+hoja.delete_rows(1, 1)
+
+wb.save("Planilla_nueva.xlsx")
