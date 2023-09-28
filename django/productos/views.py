ba3cb84504ca53bdb8851ed63d9aa6d48cd9106a
Brian Bentancourt <brian.bentancourt@sonda.com>
2023-09-28 12:00:51 -0300

    mostrando un json en /productos

--- django/productos/views.py
+++ django/productos/views.py
@@ -1,8 +1,15 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from .models import Producto
 
 # Create your views here.
 
+# /productos
+
 
 def index(request):
-    return HttpResponse('Hola mundo')
+    productos = Producto.objects.all().values()
+    # productos = Producto.objects.filter(puntaje__gte=3)
+    # productos = Producto.objects.get(id=1)
+
+    return JsonResponse(list(productos), safe=False)
