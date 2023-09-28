b955c3f75f243a4a3d6fba4543631c7df063dc72
Brian Bentancourt <brian.bentancourt@sonda.com>
2023-09-28 12:00:12 -0300

    formateando modelos para visualizarlos correctamente en el panel de admin

--- django/productos/models.py
+++ django/productos/models.py
@@ -7,6 +7,9 @@ from django.utils import timezone
 class Categoria(models.Model):
     nombre = models.CharField(max_length=255)
 
+    def __str__(self) -> str:
+        return self.nombre
+
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=255)
@@ -19,3 +22,6 @@ class Producto(models.Model):
     # SET_DEFAULT: asigna un valor por defecto
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     creado_en = models.DateTimeField(default=timezone.now)
+
+    def __str__(self) -> str:
+        return self.nombre
