724f8b0927eb1c639b723d336610c0cf07d8eb5d
Brian Bentancourt <brian.bentancourt@sonda.com>
2023-10-02 10:53:21 -0300

    agregando error 404

--- django/productos/views.py
+++ django/productos/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.http import Http404, HttpResponse, JsonResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Producto
 
 # Create your views here.
@@ -20,7 +20,8 @@ def index(request):
 
 
 def detalle(request, producto_id):
-    producto = Producto.objects.get(id=producto_id)
+    producto = get_object_or_404(Producto, id=producto_id)
+
     return render(
         request,
         'detalle.html',
